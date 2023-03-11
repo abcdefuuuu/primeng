61efcc1f081093b8734a0a7f70d6bba08d12bfc8
zowie <C109118211@nkust.edu.tw>
2023-03-12 02:15:05 +0800

    跑不出來到底為啥==

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ShareModule } from './share/share.module';
+import { FormComponent } from './form/form.component';
+import { PrimengModule } from './share/share.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ShareModule
   ],
+  exports: [
+    ShareModule,
+    PrimengModule
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
